4bddd596cebbe84222ef1d7f179777b52ffd2e3c
Lilith645 <lilith@inet-sys.com>
2016-07-15 02:52:40 +1000

    Fixed Highscore screen bug, Fixed random white dot on ship

--- include/Menus/HighscoreScreen.h
+++ include/Menus/HighscoreScreen.h
@@ -13,20 +13,20 @@ class HighscoreScreen: public Menu {
     void setup();
     void restart();
     void update(float mX, float mY, unsigned int* mouseBtnState, unsigned int* prevMouseBtnState, unsigned char* keyState, unsigned char* prevKeyState);
-   
+
     void setScore(int score);
   protected:
     void saveDefaults();
     void LoadHighscores(int score);
-    
-    void drawBackground() {  }
-    
+
+    void drawBackground() { glColor4f(0.0, 0.0,0.0,0.0f); }
+
     int highscores[10];
     std::string names[10];
-  
+
     Button Retry, Quit;
-    Label lbTitle, lbScore; 
-    Label lbHighscoreNames[10], lbHighscores[10]; 
+    Label lbTitle, lbScore;
+    Label lbHighscoreNames[10], lbHighscores[10];
 };
 
 #endif
