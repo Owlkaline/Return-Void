d05ac0aa3a8181e886233cedfa5eb60f5859c5c7
Lilith645 <lilith@inet-sys.com>
2016-07-26 15:47:20 +1000

    Removed sdl ttf stuff Changes committed: 	modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 //Using SDL and standard IO
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_ttf.h>
+//#include <SDL2/SDL_ttf.h>
 
 #ifdef __APPLE__
 #include <OpenGL/gl.h>// Header File For The OpenGL32 Library
@@ -61,8 +61,8 @@ unsigned char prevKeyState[255];
 
 GLuint mouseTexture;
 
-SDL_Surface* surface;
-TTF_Font* font;
+//SDL_Surface* surface;
+//TTF_Font* font;
 
 DisplayManager* Display[5] = { new MainMenu(), new Game(), new SettingsMenu(), new Shop(), new HighscoreScreen() };
 
@@ -71,9 +71,6 @@ SDL_Window* gWindow = NULL;
 
 //OpenGL context
 SDL_GLContext gContext;
-
-//Render flag
-bool gRenderQuad = true;
 
 void mouse() {
   int x, y;
@@ -154,16 +151,6 @@ void display() {
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
   glColor4ub(255,255,255,255); //sets full colours and alpha
-  surface  = SDL_GetWindowSurface(gWindow);
-    SDL_Color White = {0, 255, 0,255};  // this is the color in rgb format, maxing out all would give you the color white, and it will be your text's color
-
-  SDL_Surface* surfaceMessage = TTF_RenderText_Solid(font, "Testing text", White); // as TTF_RenderText_Solid could only be used on SDL_Surface then you have to create the surface first
-  SDL_Rect pos;
-  pos.x = SPACE_X_RESOLUTION/2;
-  pos.y = SPACE_Y_RESOLUTION/2;
-  pos.w = 100;
-  pos.h = 100;
-  SDL_BlitSurface(surfaceMessage, NULL, surface, &pos);
 
   Display[type]->update(mouseX, mouseY, mouseBtnState, prevMouseBtnState, keyState, prevKeyState);
   Display[type]->draw();
@@ -191,6 +178,16 @@ void display() {
     Display[type]->setup();
   }
   
+    //surface  = SDL_GetWindowSurface(gWindow);
+    //SDL_Color White = {0, 255, 0,255};  // this is the color in rgb format, maxing out all would give you the color white, and it will be your text's color
+
+  //SDL_Surface* surfaceMessage = TTF_RenderText_Solid(font, "Testing text", White); // as TTF_RenderText_Solid could only be used on SDL_Surface then you have to create the surface first
+  //SDL_Rect pos;
+ // pos.x = 500;
+ // pos.y = 500;
+ // pos.w = SPACE_X_RESOLUTION;
+ // pos.h = SPACE_Y_RESOLUTION;
+ // SDL_BlitSurface(surfaceMessage, NULL, surface, &pos);
 
  // SDL_FreeSurface(surfaceMessage); 
  
@@ -240,12 +237,12 @@ void init() {
       gWindow = SDL_CreateWindow("Return-Void", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,  screenResX, screenResY, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN );
       
     }    
-    TTF_Init();
+    //TTF_Init();
     settings.Save();  
     SDL_ShowCursor(SDL_DISABLE);
   }
   
-  surface  = SDL_GetWindowSurface(gWindow);
+  //surface  = SDL_GetWindowSurface(gWindow);
   
   aspectRatio = (float)screenResX / screenResY;
   aspectW = (float)SPACE_X_RESOLUTION/(float)screenResX;
@@ -260,9 +257,9 @@ void init() {
   
   gluOrtho2D(0.f, SPACE_X_RESOLUTION, 0.f, SPACE_Y_RESOLUTION);
   
-  font = TTF_OpenFont("lazy.ttf", 70); //this opens a font style and sets a size
-  if(font == NULL)
-    exit(0);
+  //font = TTF_OpenFont("lazy.ttf", 12); //this opens a font style and sets a size
+  //if(font == NULL)
+   // exit(0);
   mouseTexture = txt::LoadTexture("Textures/Game/Crosshair.png");
   //Initialize clear color
   glClearColor( 0.0f, 0.0f, 0.0f, 1.0f );
